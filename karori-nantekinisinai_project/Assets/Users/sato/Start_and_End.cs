08263db041992e3fc7e509cbfcf3893e38680cec
wafflecoat <TTC22C1020026@edu.tech.ac.jp>
2022-10-28 20:12:53 +0900

    制限時間と勝敗判定

--- karori-nantekinisinai_project/Assets/Users/sato/Start_and_End.cs
+++ karori-nantekinisinai_project/Assets/Users/sato/Start_and_End.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using Unity.Collections;
 using UnityEditor.EditorTools;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,11 +11,11 @@ public class Start_and_End : MonoBehaviour
 {
     public GameObject ob_fadeout;
     
-    public int winner;//１：1P勝利、２：2P勝利
+    public int winner = 0;//１：1P勝利、２：2P勝利、３：時間切れ
 
-    
-    [SerializeField] private float fadeSpeed;
-    [SerializeField] private float StartWait;
+    [SerializeField] private float FadeSpeed;
+    [SerializeField] private float Time_limit;//ゲームの制限時間、単位は秒
+
     private Image fade_panel;
     private GameObject UI_Canvas;
     private Transform TF_Canvas;
@@ -31,7 +33,7 @@ public class Start_and_End : MonoBehaviour
         fade_panel = ob_fadeout.GetComponent<Image>();
         number_text = new GameObject[3];
 
-        //１～３のイメージを取得
+        //１～３の絵を取得
         for (int i = 0; i < 3; i++)
         {
             number_text[i] = TF_Canvas.GetChild(i).gameObject;
@@ -39,7 +41,6 @@ public class Start_and_End : MonoBehaviour
         Start_text = TF_Canvas.GetChild(3).gameObject;
         GameSet_text = TF_Canvas.GetChild(4).gameObject;
         TimeUp_text = TF_Canvas.GetChild(5).gameObject;
-
     }
     // Start is called before the first frame update
     IEnumerator Start()
@@ -47,76 +48,110 @@ public class Start_and_End : MonoBehaviour
         //画面が黒からフェードイン
         yield return StartCoroutine(fadein());
 
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
         //３カウント後スタートの表示
         yield return StartCoroutine(Start_Count());
+        //制限時間のカウントスタート
+        StartCoroutine(Time_Count());
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButtonDown("Jump"))
-        {
-            Debug.Log("aaa");
-            StartCoroutine(fadeout());
-            //SceneManager.LoadScene("main");
-        }
-    }
-
+    //落ちた時の勝敗判定
     private void OnTriggerEnter(Collider player)
     {
 
-        if(player.gameObject.CompareTag("Player"))
-        {
-            winner = 1;
-            StartCoroutine(fadeout());
-        }
-        else if (player.gameObject.CompareTag("gruound"))
+        if(player.gameObject.CompareTag("Player") && winner == 0 )
         {
             winner = 2;
-            StartCoroutine(fadeout());
+            Debug.Log("1");
+            StartCoroutine(GameEnd());
+        }
+        else if (player.gameObject.CompareTag("Player2") && winner == 0)
+        {
+            winner = 1;
+            Debug.Log("2");
+            StartCoroutine(GameEnd());
         }
     } 
 
+   
     IEnumerator fadein()
     {
+        var wait = new WaitForEndOfFrame();
         while (true)
         {
-            yield return new WaitForEndOfFrame();
+            yield return wait;
             Color color = fade_panel.color;
-            color.a -= fadeSpeed * Time.deltaTime;
+            color.a -= FadeSpeed * Time.deltaTime;
             fade_panel.color = color;
             if (color.a <= 0) break;
-            Debug.Log("b");
         }
-        Debug.Log("a");
     }
 
     IEnumerator fadeout()
     {
+        var wait = new WaitForEndOfFrame();
         while (true)
         {
-            yield return new WaitForEndOfFrame();
+            yield return wait;
             Color color = fade_panel.color;
-            color.a += fadeSpeed * Time.deltaTime;
+            color.a += FadeSpeed * Time.deltaTime;
             fade_panel.color = color;
             if (color.a >= 1) break;
         }
-        Debug.Log("a");
     }
 
     IEnumerator Start_Count()
     {
+        var wait = new WaitForSeconds(1f);
         for(int i = 0; i < 3; i++)
         {
             number_text[i].SetActive(true);
-            yield return new WaitForSeconds(1f);
+            yield return wait;
             number_text[i].SetActive(false);
         }
 
         Start_text.SetActive(true);
-        yield return new WaitForSeconds(1f);
+        yield return wait;
         Start_text.SetActive(false);
+    }
+
+    IEnumerator Time_Count()
+    {
+        var wait = new WaitForFixedUpdate();//FixedUpdateはデフォルトで0.02秒ごとに呼ばれる
+        float last_time = Time_limit;
+        Time_draw();
+        while (0 < Time_limit) 
+        {
+            yield return wait;
+            Time_limit -= 0.02f;
+            //一秒ごとに時間の表示を‐１
+            if(last_time - Time_limit >= 1)
+            {
+                //制限時間表示の変更
+                Time_draw();
+                last_time = Time_limit;
+            }
+        }
+        StartCoroutine(GameEnd());
+    }
+
+    private void Time_draw()
+    {
+        double game_time = Math.Ceiling(Time_limit) - 1;
+        int minutes = (int)game_time / 60;
+        int seconds = (int)game_time % 60;
+        Debug.Log(minutes + ":" + seconds / 10 + seconds % 10);
+    }
+
+    IEnumerator GameEnd()
+    {
+        StopCoroutine(Time_Count());
+        if (winner == 3) TimeUp_text.SetActive(true);
+        else GameSet_text.SetActive(true);
+
+        yield return new WaitForSeconds(1.5f);
+        yield return StartCoroutine(fadeout());
+        //SceneManager.LoadScene("Title_scene");
     }
 }
