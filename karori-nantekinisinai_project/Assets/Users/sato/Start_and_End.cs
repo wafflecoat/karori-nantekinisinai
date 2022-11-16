fd30f9164d3f6ad0e2302827a1e5c946dada0f83
wafflecoat <TTC22C1020026@edu.tech.ac.jp>
2022-11-16 22:15:07 +0900

    ビルド設定変更

--- karori-nantekinisinai_project/Assets/Users/sato/Start_and_End.cs
+++ karori-nantekinisinai_project/Assets/Users/sato/Start_and_End.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using Unity.Collections;
-using UnityEditor.EditorTools;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
